6e87c066e80c0583b471fc3af08d0f0b96a75c8e
Nadheer Chatharoo <nchatharoo@icloud.com>
2020-08-11 14:30:54 +0200

    Create the Canvas as a UIViewRepresentable

---
+++ SimpleDrawing/CanvasView.swift
@@ -0,0 +1,22 @@
+//
+//  CanvasView.swift
+//  SimpleDrawing
+//
+//  Created by Chatharoo on 11/08/2020.
+//  Copyright © 2020 Chatharoo. All rights reserved.
+//
+
+import SwiftUI
+import PencilKit
+
+
+struct Canvas: UIViewRepresentable {
+    @State var canvasView: PKCanvasView
+    
+    func makeUIView(context: Context) -> PKCanvasView {
+        self.canvasView.tool = PKInkingTool(.pen, color: .black, width: 15)
+        return canvasView
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) { }
+}
